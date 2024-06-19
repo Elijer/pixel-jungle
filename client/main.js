4c3af9aa33bd4804c8a9ec817c6c8d779f510c13
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-19 10:45:20 -0400

    client detects production env and uses hardcoded heroku url

--- client/main.js
+++ client/main.js
@@ -3,13 +3,18 @@ import { v4 as uuidv4 } from 'uuid';
 
 const playerId = () => localStorage.getItem('playerId') || localStorage.setItem('playerId', uuidv4())
 
-let port = process.env.PORT ? process.env.PORT : '3000'
-let baseURL = process.env.PROD ? 'https://saskanupe-b0a033b8892a.herokuapp.com/' : "localhost"
-let socketAddress = `ws://${baseURL}:${port}`
+// let port = process.env.PORT ? process.env.PORT : '3000'
+// let baseURL = process.env.PROD ? 'https://saskanupe-b0a033b8892a.herokuapp.com/' : "localhost"
+// let socketAddress = `ws://${baseURL}:${port}`
 // if (process.env.BASE_URL && process.env.PORT){
 //   socketAddress = `ws://${process.env.BASE_URL}:${port}`
 //   console.log(`Attempting to connect to ${socketAddress}`)
 // }
+
+const socketAddress = "ws://localhost:3000"
+if (window.location.hostname !== "localhost"){
+  socketAddress = "https://saskanupe-b0a033b8892a.herokuapp.com/"
+}
 
 const  socket = io(socketAddress)
 
