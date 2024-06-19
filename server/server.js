abcebdbb99661d4055e96083e0db0a6d77f8d48e
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-18 21:01:27 -0400

    created a way to serve client from the server itself

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const httpServer = createServer();
+// ES modules compatible way to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(express.static(path.join(__dirname, 'client/dist'))); // Adjust the path to your client build directory
 
 console.log("server started");
 
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
@@ -19,6 +30,11 @@ io.on("connection", (socket) => {
     console.log("Message received:", msg);
     io.emit("hi", msg + " -> signed by server");
   });
+});
+
+// Serve the client HTML file for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html')); // Adjust the path to your index.html
 });
 
 httpServer.listen(3000, () => {
