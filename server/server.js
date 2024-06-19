9964af7b5d9b64ed2561429c09631563fcb03c57
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-19 15:12:52 -0400

    trib

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// ES modules compatible way to get __dirname
+// HELLO
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
